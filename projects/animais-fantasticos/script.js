a54301003653f487954a99e0819b75c860dae813
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-02-14 20:41:06 -0300

    DOM e js

--- projects/animais-fantasticos/script.js
+++ projects/animais-fantasticos/script.js
@@ -1,19 +1,25 @@
-let titulos = document.querySelectorAll(".titulo");
-let menu = document.querySelector(".menu");
+// let titulos = document.querySelectorAll(".titulo");
+// let menu = document.querySelector(".menu");
 
-menu.addEventListener("click", () => {
-  titulos.forEach((titulo) => {
-    titulo.innerText = "ULALAU";
-  });
-});
+// menu.addEventListener("click", () => {
+//   titulos.forEach((titulo) => {
+//     titulo.innerText = "ULALAU";
+//   });
+// });
 
-let see = true;
-let faq = document.querySelector(".faq-lista");
-let cliqueFaq = document.querySelector(".faq .titulo");
+// let see = true;
+// let faq = document.querySelector(".faq-lista");
+// let cliqueFaq = document.querySelector(".faq .titulo");
 
-cliqueFaq.addEventListener("click", () => {
-  if (faq) {
-    see = !see;
-    faq.style.display = see ? "block" : "none";
-  }
-});
+// cliqueFaq.addEventListener("click", () => {
+//   if (faq) {
+//     see = !see;
+//     faq.style.display = see ? "block" : "none";
+//   }
+// });
+
+const animais = document.getElementById("animais");
+console.log(animais);
+
+const gridSection = document.getElementsByClassName("grid-section");
+console.log(gridSection);
