1e1e69e0e4bee4177ee5301678657f5564f59220
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-02-24 18:56:18 -0300

    Atividade height e width

--- projects/animais-fantasticos/atividade.js
+++ projects/animais-fantasticos/atividade.js
@@ -101,3 +101,67 @@ function attributes() {
   //
 }
 // attributes();
+
+function windoww() {
+  const primeiroH2 = document.querySelector("h2");
+  const h2rect = primeiroH2.getBoundingClientRect();
+
+  console.log(h2rect.top);
+
+  if (h2rect.top < 0) {
+    console.log("passou do elemento");
+  }
+
+  //Verifique a distância da primeira imagem em relação ao topo da págine
+  const img = document.querySelector("img");
+  const rect = img.getBoundingClientRect();
+
+  console.log(rect.top);
+
+  //OUUUU
+
+  const imgTop = img.offsetTop;
+  console.log(imgTop);
+
+  //Retorne a soma da largura de todas as imagens
+  function somaImagens() {
+    const imgs = document.querySelectorAll("img");
+    let soma = 0;
+
+    imgs.forEach((imagem) => {
+      soma += imagem.offsetWidth;
+    });
+    console.log(soma);
+  }
+
+  window.onload = function () {
+    somaImagens();
+  };
+
+  // Verifique se os links da página possuem o mínimo
+  // recomendado para telas utilizadas com o dedo (48px/48px de acordo com o google)
+  const links = document.querySelectorAll("a");
+
+  links.forEach((link) => {
+    const linkWidth = link.offsetWidth;
+    const linkHeight = link.offsetHeigth;
+
+    if (linkWidth > 48 && linkHeight > 48) {
+      console.log("Possui acessilibidade");
+    } else {
+      console.log("Não possui boa acessibilidade");
+    }
+  });
+
+  //Se o browser for menor que 720px add a classe menu-mobile ao menu
+
+  const browserSmall = window.matchMedia("(max-width: 720px)").matches;
+
+  const menu = document.querySelector(".menu");
+
+  if (browserSmall) {
+    menu.classList.add("menu-mobile");
+  }
+  console.log(menu);
+}
+windoww();
