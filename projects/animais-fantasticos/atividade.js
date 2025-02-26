ec4d91161cc2ce5cee71c702b8a9b406ce3087db
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-02-25 23:39:21 -0300

    Atividade sobre Eventos no projeto Animais Fantásticos

--- projects/animais-fantasticos/atividade.js
+++ projects/animais-fantasticos/atividade.js
@@ -174,3 +174,62 @@ img.addEventListener("click", () => {
 });
 // }
 // evento();
+
+function eventos() {
+  // Quando so usuários clicarem nos links internos do site
+  // adicione a classe 'ativo' ao item clicado e remova dos demais itens caso eles já possuam.
+  // Previna o comportamento padrão dos links
+
+  const linksInternos = document.querySelectorAll('a[href^="#"]');
+  // console.log(linksInternos);
+
+  function handleClick(event) {
+    event.preventDefault();
+
+    linksInternos.forEach((link) => {
+      link.classList.remove("ativo");
+    });
+
+    event.currentTarget.classList.add("ativo");
+    // ou: this.classList.add("ativo");
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", handleClick);
+  });
+
+  // Selecione todos os elementos do site começando a partir do body,
+  //ao clique mostre extamente quais elementos estão sendo clicados
+
+  const todosElementos = document.querySelectorAll("body *");
+
+  function handleElement(event) {
+    console.log(event.currentTarget);
+  }
+
+  todosElementos.forEach((elemento) => {
+    elemento.addEventListener("click", handleElement);
+  });
+  // Ao ionvés de mostrar no console, remova o elemento que está sendo clicado, o método remove() remove um elemento.
+  // const todosElementos = document.querySelectorAll("body *");
+
+  // function handleElement(event) {
+  //   event.currentTarget.remove();
+  // }
+
+  // todosElementos.forEach((elemento) => {
+  //   elemento.addEventListener("click", handleElement);
+  // });
+
+  // Se um usuário clicar na tecla 'T' aumente todo o texto do site
+  function handleClickT(event) {
+    console.log(event.key);
+    if (event.key === "t") {
+      document.documentElement.classList.toggle("textomaior");
+      // document.documentElement fala diretamente com o html
+    }
+  }
+
+  window.addEventListener("keydown", handleClickT);
+}
+eventos();
