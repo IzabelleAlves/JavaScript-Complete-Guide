8a04bf77c4542aa94b6ef3598be611b794185285
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-03-05 16:02:25 -0300

    Atividade sobre Protótipo

--- intermediate/objetos/prototype/script.js
+++ intermediate/objetos/prototype/script.js
@@ -1,8 +1,3 @@
-function Pessoa(nome, idade) {
-  this.nome = nome;
-  this.idade = idade;
-}
-
 const eu = new Pessoa("Izabelle", 22);
 
 eu.andar = function () {
@@ -14,3 +9,34 @@ console.log(eu.prototype);
 Pessoa.prototype.andar = function () {
   return this.nome + " Pessoa andou";
 };
+
+// crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+
+function Pessoa(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+// crie um método no protótipo e retorne o nome completo das pessoas
+Pessoa.prototype.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome} ${this.idade}`;
+};
+
+let iza = new Pessoa("Izabelle", "Alves", 22);
+
+// liste os métodos acessaos por daos criados com NodeList, HTMLCollection, Document
+
+// liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+li;
+li.click;
+li.innerText;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+// qual o constructor do dado abaixo?
+li.hidden.constructor.name;
